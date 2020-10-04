3fc9899f8027c71ebde9c225cecf76d296e73a12
unknown <raaoemc@gmail.com>
2020-10-04 00:21:14 -0700

    initial commit

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Bird : MonoBehaviour {
+    // Movement speed
+    public float speed = 2;
+    
+    // Flap force
+    public float force = 300;
+
+    // Use this for initialization
+    void Start () {    
+        // Fly towards the right
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * speed;
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        // Flap
+        if (Input.GetKeyDown(KeyCode.Space))
+            GetComponent<Rigidbody2D>().AddForce(Vector2.up * force);
+
+        //Debug.Log(transform.position.x + " x coord");
+        //Debug.Log(transform.position.y + " y coord");
+        //Debug.Log(transform.position.z + " z coord");
+    }
+    
+    void OnCollisionEnter2D(Collision2D coll) {
+        // Restart
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
